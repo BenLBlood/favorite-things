33e0b151be70e231af96e4e5e0a827ca25d425cd
Ben Blood <bluealikai@gmail.com>
2018-02-07 23:14:33 -0500

    favorite color done

--- FavoriteThings/src/app/app.component.ts
+++ FavoriteThings/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { filter } from 'rxjs/operator/filter';
 
 import * as firebase from 'firebase/app';
+import { OnInit, OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 
 @Component({
@@ -10,7 +11,8 @@ import * as firebase from 'firebase/app';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
   title = 'Favorite Things';
   favoriteColor = 'pink';
   favoriteNumber = 0;
@@ -19,17 +21,27 @@ export class AppComponent {
 
   }
 
+  ngOnInit(): void {
+    firebase.database().ref().child('color').on('value',
+      (snapshot: firebase.database.DataSnapshot) => {
+        this.favoriteColor = snapshot.val();
+      })
+  }
+  ngOnDestroy(): void {
+    firebase.database().ref().child('color').off();
+  }
+
   setColor(selectedColor: string): void {
     firebase.database().ref().child('color').set(selectedColor);
   }
 
-  updateColor(): void {
-    console.log('TODO: Update the color');
-    firebase.database().ref().child('color').once('value',
-      (snapshot: firebase.database.DataSnapshot) => {
-        this.favoriteColor = snapshot.val(); 
-      })
-  }
+  // updateColor(): void {
+  //   console.log('TODO: Update the color');
+  //   firebase.database().ref().child('color').once('value',
+  //     (snapshot: firebase.database.DataSnapshot) => {
+  //       this.favoriteColor = snapshot.val(); 
+  //     })
+  // }
 
   setNumber(newNumber: number): void {
     this.favoriteNumber = newNumber;
